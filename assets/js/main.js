381a076538a61a012bb14771a557efb3fb88b786
lukelyyeung <lukelyyeung@gmail.com>
2018-06-16 12:19:41 +0800

    1st commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,63 @@
 Vue.component('sharedlink', {
     props: ['link'],
-    template: '<li>{{link.url}}</li>'
-})
+    template:
+        `<li>
+        <span> {{link.url}} </span>
+        <span> {{ link.tags }} </span>
+    </li>`
+});
+
+Vue.component('taginput', {
+    props: ['tags', 'index'],
+    template:
+        `<li>
+        <input v-model="tags[index]">
+    </li>`
+});
 
 const app = new Vue({
     el: '#app',
-    data: {
-        links: [
-            { id: 0, url: 'www.yahoo.com' },
-            { id: 1, url: 'www.google.com' },
-            { id: 2, url: 'www.whatsapp.com' }
-        ],
-        inputLink: ''
+    data() {
+        return {
+            links: [
+                { id: 0, url: 'www.yahoo.com', tags: ['yahoo'] },
+                { id: 1, url: 'www.google.com', tags: [] },
+                { id: 2, url: 'www.whatsapp.com', tags: [] }
+            ],
+            input: {
+                url: '',
+                tags: []
+            },
+            keyword: ''
+        }
     },
     methods: {
         submitLink: function () {
+            this.links.push({
+                id: this.links.length,
+                url: this.input.url,
+                tags: this.input.tags
+            })
         },
-        clear: function() {
-            this.inputLink = ''
+        addTag: function () {
+            this.input.tags.push('');
+        },
+        clear: function () {
+            this.input = {
+                url: '',
+                tags: []
+            }
+        }
+    },
+    computed: {
+        filteredLinks: function() {
+            const regKeyword = RegExp(this.keyword, 'g');
+
+            return this.links.filter(link => {
+                if (link.url.match(regKeyword) || link.tags.some(tag => tag.match(regKeyword))) {
+                    return link;
+                }
+            });
         }
     }
 })
